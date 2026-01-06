64c4e9ff734f3e071eaacf4d8c6b3ff4fbe7d794
lawrenceHD <haroldtetchouang@gmail.com>
2026-01-06 01:52:40 +0100

    first commit

---
+++ src/main/java/com/hexalib/hexalib_api/HexalibApiApplication.java
@@ -0,0 +1,13 @@
+package com.hexalib.hexalib_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexalibApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexalibApiApplication.class, args);
+	}
+
+}
